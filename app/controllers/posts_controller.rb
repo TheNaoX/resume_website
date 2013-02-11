ac74762f12e911d57473d07280cd28e652187d3e
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-02-11 13:05:48 -0600

    added more behavior

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,27 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
   def index
     @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
   
   def create
+    @post = Post.new(params[:post].merge(author_id: current_user.id))
+    respond_to do |format|
+      if @post.save
+        flash[:success] = "Successfully created new post #{@post.title}"
+        format.html { redirect_to posts_path }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
   
 end

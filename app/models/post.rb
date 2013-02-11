9a3d7206e759ae49ffb5ac2e86c8df263fcc879d
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-02-11 13:35:04 -0600

    added content tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ActiveRecord::Base
-  attr_accessible :author_id, :content, :title
+
+  attr_accessible :author_id, :content, :title, :link
 
   belongs_to :author, class_name: User, foreign_key: :author_id
 
   validates_presence_of :content, :title, :author_id
   validates :content, length: { minimum: 140, message: "Please be more descriptive with the message" }
   validates :title, uniqueness: { message: "This post already exists" }
+  validates :link, format: { with: URI::regexp(%w(http https)), message: "Please enter valid link" }
+
+  default_scope order('created_at DESC')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211182958) do
+ActiveRecord::Schema.define(:version => 20130211190724) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130211182958) do
     t.integer  "author_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "link"
   end
 
   create_table "users", :force => true do |t|

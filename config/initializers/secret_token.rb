2409575510eeaa189e761909272afe8759b1a653
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-02-11 11:53:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainessExam::Application.config.secret_token = '6643a11d25db6e1c665cd01f1c47472f9110488c46d53d18950c786121d8bfa36e5006cf2b542989cc06a2290d5f365c854967fd2b5c71d578ff5dd700164836'
